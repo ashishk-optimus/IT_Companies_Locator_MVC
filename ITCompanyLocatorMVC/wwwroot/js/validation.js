45fdf95c457a9b5c1a7dda69f0590108892d2739
Ashish Kumar <ashishk-optimus@optimusinfo.com>
2018-03-10 16:56:00 +0530

    Client side validation added

--- ITCompanyLocatorMVC/wwwroot/js/validation.js
+++
@@ -1,9 +0,0 @@
-﻿function validateCity() {
-    var location = document.forms["searchForm"]["cityName"].value;
-    var format = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
-
-    if (location.match(format)) {
-        alert("Special characters are not allowed..!!!");
-        return false;
-    }
-}
